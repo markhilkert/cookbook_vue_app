99bb861e9fe217f460e533e028523cc01bb225ce
markhilkert <mark.hilkert@gmail.com>
2019-02-19 10:08:48 -0600

    added update button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -28,12 +28,37 @@
       <h2>{{ recipe.title }}</h2>
       <img v-bind:src="recipe.image_url" v-bind:alt="recipe.title">
       <div>
-        <button v-on:click="currentRecipe = recipe">More Info</button>  <!-- Changes currentRecipe to recipe when you click -->
+        <button v-on:click="showRecipe(recipe)">More Info</button>  <!-- Changes currentRecipe to recipe when you click -->
       </div>
       <div v-if="recipe === currentRecipe">  <!-- turns on/off depending on the value of currentRecipe -->
         <p> Prep Time: {{ recipe.prep_time }} </p>
         <p> Ingredients: {{ recipe.ingredients }} </p>
         <p> Directions: {{ recipe.directions }} </p>
+
+        <div>
+          <h4>Edit Recipe</h4>
+          <div>
+            <div>
+              Title: <input v-model="recipe.title">
+            </div>
+            <div>
+              Chef: <input v-model="recipe.chef">
+            </div>
+            <div>
+              Prep Time: <input v-model="recipe.prep_time">
+            </div>
+            <div>
+              Ingredients: <input v-model="recipe.ingredients">
+            </div>
+            <div>
+              Directions: <input v-model="recipe.directions">
+            </div>
+            <div>
+              Image URL: <input v-model="recipe.image_url">
+            </div>
+            <button v-on:click="updateRecipe(recipe)">Update Recipe</button>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -67,6 +92,13 @@ export default {
       });
   },
   methods: {
+    showRecipe: function(inputRecipe) {
+      if (this.currentRecipe === inputRecipe) {
+        this.currentRecipe = {};
+      } else {
+        this.currentRecipe = inputRecipe;
+      }
+    },
     createRecipe: function() {
       console.log("Create the Recipe...")
       var params = {
@@ -82,6 +114,20 @@ export default {
           console.log("Success", response.data);
           this.recipes.push(response.data);
         });
+    },
+    updateRecipe: function(inputRecipe) {
+      var params = {
+                    title: inputRecipe.title,
+                    chef: inputRecipe.chef,
+                    prep_time: inputRecipe.prep_time,
+                    ingredients: inputRecipe.ingredients,
+                    directions: inputRecipe.directions,
+                    image_url: inputRecipe.image_url
+                    };
+      axios.patch("/api/recipes/" + inputRecipe.id, params)
+      .then( response => {
+        console.log("Recipe Successfully Updated", response.data);
+      });
     }
   }
 };
